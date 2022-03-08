f4c5f8ff00ebcd847a6663eb1969d236120d0d73
richardbrks <richardbrks@gmail.com>
2022-03-08 11:57:29 -0500

    update

--- +bindings.el
+++
@@ -1,9 +0,0 @@
-;;; ~/.doom.d/+bindings.el -*- lexical-binding: t; -*-
-
-;; overrides other minor mode keymaps (just for non-evil)
-(map!
-  ;; Conventional text-editing keys & motions
- "M-a" #'mark-whole-buffer
- "M-left"      #'backward-word
- "M-right"     #'forward-word
- )
